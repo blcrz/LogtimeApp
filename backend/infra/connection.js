012af8f7734eb59c3943e3c737960930aa9f83c8
Bárbara Cruz <ba.luizacruz@gmail.com>
2022-01-10 12:40:43 -0300

    fix: adicionado banco de dados remoto.

--- backend/infra/connection.js
+++ backend/infra/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'us-cdbr-east-05.cleardb.net',
     port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'timeTrackingAPP'
+    user: 'ba6f70d78bcd83',
+    password: '99b99ffd',
+    database: 'heroku_c6f7beb8dde02ce'
 })
 
 module.exports = connection
